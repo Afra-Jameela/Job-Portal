773bb7ba9cc853c5b45373ac836dab33efe2ced7
Afra-Jameela <afrajameela01@gmail.com>
2024-10-27 17:42:20 +0530

    first commit

--- System/job-list.php
+++ System/job-list.php
@@ -159,7 +159,7 @@ $title = "Job List";
 			
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title text-center">Create your account for free</h4>
+					<h4 class="modal-title text-center">Create your account</h4>
 				</div>
 				
 				<div class="modal-body">
